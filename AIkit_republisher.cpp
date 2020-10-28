3480162e001b7eb0869ba2a343c47e82efa11e25
Cesar Abraham <cesam98@gmail.com>
2020-10-27 21:15:09 -0700

    Update AIkit_republisher.cpp

--- AIkit_republisher.cpp
+++ AIkit_republisher.cpp
@@ -1,13 +1,26 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 
+double imuValues1[4] = {0 0 0 1};
+double imuValues2[4] = {0 0 0 1};
+double imuValues3[4] = {0 0 0 1};
+double imuValues4[4] = {0 0 0 1};
+
+void AIkit1Callback(const sensor_msgs::Imu::ConstPtr &values)
+{
+  imuValues1[0] = values->orientation.x;
+  imuValues1[1] = values->orientation.y;
+  imuValues1[2] = values->orientation.z;
+  imuValues1[3] = values->orientation.w;
+
+}
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "AIkit_republisher");
   ros::NodeHandle n;
 
   ros::Rate r(50);
-  ros::Subscriber AIkit1_sub = n.subscribe("sensorTopic",10,AIkitCallback);
+  ros::Subscriber AIkit1_sub = n.subscribe("sensorTopic",10,AIkit1Callback);
   while(n.ok()){
     r.sleep();
   }
