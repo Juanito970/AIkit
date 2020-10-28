a592fdd697df36073cbe6030b8a31123020e3dc7
Cesar Abraham <cesam98@gmail.com>
2020-10-27 20:32:15 -0700

    Update AIkit_republisher.cpp

--- AIkit_republisher.cpp
+++ AIkit_republisher.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
 
 
 int main(int argc, char** argv){
@@ -6,7 +7,7 @@ int main(int argc, char** argv){
   ros::NodeHandle n;
 
   ros::Rate r(50);
-
+  ros::Subscriber AIkit1_sub = n.subscribe("sensorTopic",10,AIkitCallback);
   while(n.ok()){
     r.sleep();
   }
