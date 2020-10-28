2fa7e25e4b04cb5f34e0f8427a9992a962f573eb
Cesar Abraham <cesam98@gmail.com>
2020-10-27 21:43:38 -0700

    Update AIkit_republisher.cpp

--- AIkit_republisher.cpp
+++ AIkit_republisher.cpp
@@ -1,18 +1,25 @@
 #include <ros/ros.h>
+#include <tf/tf.h>
 #include <sensor_msgs/Imu.h>
 
-double imuValues1[4] = {0 0 0 1};
+/*double imuValues1[4] = {0 0 0 1};
 double imuValues2[4] = {0 0 0 1};
 double imuValues3[4] = {0 0 0 1};
-double imuValues4[4] = {0 0 0 1};
+double imuValues4[4] = {0 0 0 1};*/
+tf::Quaternion q1(0,0,0,1);
+tf::Matrix3x3 m1(q1);
 
 void AIkit1Callback(const sensor_msgs::Imu::ConstPtr &values)
 {
-  imuValues1[0] = values->orientation.x;
+  /*imuValues1[0] = values->orientation.x;
   imuValues1[1] = values->orientation.y;
   imuValues1[2] = values->orientation.z;
-  imuValues1[3] = values->orientation.w;
-
+  imuValues1[3] = values->orientation.w;*/
+  q1 = values->orientation;
+  tf::Matrix3x3 m(q1);
+  m = m.transpose();
+  m = {m[2],m[0],m[1]};
+  m1 = m.transpose();
 }
 
 int main(int argc, char** argv){
